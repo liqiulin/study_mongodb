b4f386dbebdbdb43d7fd92a5586dadb78886460d
liqiulin <liqiulin@fxiaoke.com>
2017-03-09 17:00:39 +0800

    返回查询的映射字段

--- morphia-example/src/main/java/org/mongodb/morphia/example/coverindex/StudentDAO.java
+++ morphia-example/src/main/java/org/mongodb/morphia/example/coverindex/StudentDAO.java
@@ -10,6 +10,11 @@ import java.util.List;
 public interface StudentDAO {
     ObjectId save(Student student);
 
+    /**
+     *  返回查询的映射字段   http://docs.mongoing.com/manual-zh/tutorial/project-fields-from-query-results.html
+     * @param name
+     * @return
+     */
     List<Student> queryByNameWithoutId(String name);
 
     List<Student> QueryByNameWithCoverIndex(String name);
